671177a83667ff09ef94cd338d3f78a61b4bb785
Vas <pdforigami@gmail.com>
2021-10-25 17:33:54 +0300

    Create error handling controller/middleware

---
+++ controllers/errorController.js
@@ -0,0 +1,8 @@
+export default (err, req, res, next) => {
+  res.status(err.statusCode || 500).json({
+    status: err.status || 'error',
+    error: err,
+    message: err.message,
+    stack: err.stack,
+  });
+};
