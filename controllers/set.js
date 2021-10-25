8edc51b60338c711687197c9cbf7d3d531355b4a
Vas <pdforigami@gmail.com>
2021-10-25 16:59:51 +0300

    Start moving set business logic to /controllers

---
+++ controllers/set.js
@@ -0,0 +1,75 @@
+import query from '../db-connect.js';
+
+async function getAll(req, res, next) {
+  try {
+    const queryResult = await query('SELECT * FROM set');
+    res.status(200).json(queryResult.rows);
+  } catch (error) {
+    next(error);
+  }
+}
+
+async function getOne(req, res, next) {
+  try {
+    const queryResult = await query(
+      'SELECT * FROM set WHERE id = $1',
+      [req.params.set_id],
+    );
+    res.status(200).json(queryResult.rows[0]);
+  } catch (error) {
+    next(error);
+  }
+}
+
+async function createOne(req, res, next) {
+  try {
+    await query(
+      'INSERT INTO set (name, description) VALUES ($1, $2)',
+      [req.body.name, req.body.description],
+    );
+    res.status(201).end();
+  } catch (error) {
+    next(error);
+  }
+}
+
+async function getEntities(req, res, next) {
+  try {
+    const queryResult = await query(
+      `SELECT e.*, a.name AS action_name, s_e_a.score
+      FROM entity e
+      INNER JOIN set_entity s_e
+      ON e.id = s_e.entity_id
+      INNER JOIN set_entity_action s_e_a
+      ON e.id = s_e_a.entity_id
+      INNER JOIN action a
+      ON s_e_a.action_id = a.id
+      WHERE s_e.set_id = $1`,
+      [req.params.set_id],
+    );
+
+    const entities = queryResult.rows.filter((row, index) => (
+      queryResult.rows.findIndex(gfy => gfy.id === row.id) === index
+    ));
+
+    queryResult.rows.map(row => {
+      const entity = entities.find(e => e.id === row.id);
+      if (!('scores' in entity)) {
+        entity.scores = {};
+      }
+      entity.scores[row.action_name] = row.score;
+      return entity;
+    });
+
+    res.status(200).json(entities);
+  } catch (error) {
+    next(error);
+  }
+}
+
+export default {
+  getAll,
+  getOne,
+  createOne,
+  getEntities,
+};
--- routes/set.js
+++ routes/set.js
@@ -1,11 +1,12 @@
 import { Router } from 'express';
+import setController from '../controllers/set.js';
 import query from '../db-connect.js';
 
 const router = Router();
 
 router.param('set_id', (req, res, next, id) => {
   query('SELECT * FROM set WHERE id = $1', [id])
-    .then((queryResult) => {
+    .then(queryResult => {
       if (queryResult.rowCount === 0) {
         res.sendStatus(404);
       } else {
@@ -18,7 +19,7 @@ router.param('set_id', (req, res, next, id) => {
 
 router.param('entity_id', (req, res, next, id) => {
   query('SELECT * FROM entity WHERE id = $1', [id])
-    .then((queryResult) => {
+    .then(queryResult => {
       if (queryResult.rowCount === 0) {
         res.sendStatus(404);
       } else {
@@ -30,55 +31,17 @@ router.param('entity_id', (req, res, next, id) => {
 });
 
 router.route('/')
-  .get((req, res) => {
-    query('SELECT * FROM set')
-      .then((queryResult) => res.json(queryResult.rows));
-  })
-  .post((req, res) => {
-    query('INSERT INTO set (name, description) VALUES ($1, $2)', [req.body.name, req.body.description])
-      .then(() => res.status(201).end());
-  });
+  .get(setController.getAll)
+  .post(setController.createOne);
 
-router.get('/:set_id', (req, res) => {
-  query('SELECT * FROM set WHERE id = $1', [req.params.set_id])
-    .then((queryResult) => {
-      res.json(queryResult.rows[0]);
-    });
-});
+router.route('/:set_id')
+  .get(setController.getOne);
 
 router.route('/:set_id/entities')
-  .get((req, res) => {
-    query(
-      `SELECT e.*, a.name AS action_name, s_e_a.score
-      FROM entity e INNER JOIN set_entity s_e
-      ON e.id = s_e.entity_id
-      INNER JOIN set_entity_action s_e_a
-      ON e.id = s_e_a.entity_id
-      INNER JOIN action a
-      ON s_e_a.action_id = a.id
-      WHERE s_e.set_id = $1`,
-      [req.params.set_id],
-    ).then((queryResult) => {
-      const entities = []
-      for (const row of queryResult.rows) {
-        if (entities.map(entity => entity.id).includes(row.id)) {
-          // Add the action and its score to .scores
-          const entity = entities.find(entity => entity.id === row.id);
-          entity.scores[row.action_name] = row.score;
-        } else {
-          // Add .scores and delete unnecessary keys
-          row.scores = { [row.action_name]: row.score }
-          delete row.score;
-          delete row.action_name;
-          entities.push(row);
-        }
-      };
-      res.json(entities);
-    });
-  });
+  .get(setController.getEntities);
 
 router.route('/:set_id/entities/:entity_id')
-  .put((req, res) => {
+  .put((req, res, next) => {
     query(
       `INSERT INTO set_entity
       VALUES ($1, $2)
@@ -86,9 +49,10 @@ router.route('/:set_id/entities/:entity_id')
       DO NOTHING`,
       [req.params.set_id, req.params.entity_id])
       .then(() => res.status(201).end())
-      .catch((error) => {
-        res.status(400).end();
-        console.log(error);
+      .catch(error => {
+        // res.status(400).end();
+        next('Error lol');
+        // console.log(error);
       });
   })
   .delete((req, res) => {
@@ -103,7 +67,7 @@ router.post('/:set_id/entities/:entity_id/vote', (req, res) => {
     SET score = score + 1 
     WHERE set_id = $1 AND entity_id = $2`,
     [req.params.set_id, req.params.entity_id],
-  ).then((queryResult) => {
+  ).then(queryResult => {
     res.json(queryResult.rows);
   });
 });
