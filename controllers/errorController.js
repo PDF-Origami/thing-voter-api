14db3bb681de8cdcd266865c165c4679304d16c1
Vas <pdforigami@gmail.com>
2021-10-25 19:04:12 +0300

    Delete error controller

--- controllers/errorController.js
+++
@@ -1,8 +0,0 @@
-export default (err, req, res, next) => {
-  res.status(err.statusCode || 500).json({
-    status: err.status || 'error',
-    error: err,
-    message: err.message,
-    stack: err.stack,
-  });
-};
