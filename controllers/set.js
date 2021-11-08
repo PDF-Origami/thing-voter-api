2c610e89828e1b0083a548bf44127b73960cc92f
Vas <pdforigami@gmail.com>
2021-11-08 23:50:55 +0200

    Remove dead code

--- controllers/set.js
+++ controllers/set.js
@@ -1,25 +1,6 @@
-import { db, QueryResultError } from '../utils/db.js';
+import { db } from '../utils/db.js';
 
-// Todo: factor out common code in addEntity, addAction, removeEntity, removeAction
-
-// Check that the thing (set, entity or action) with the given id exists,
-// then cache it in req
-async function validateParameter(req, res, next, id, parameter) {
-  const table = parameter.slice(0, -3);
-  try {
-    req[table] = await db.one(
-      'SELECT * FROM $1:name WHERE id = $2',
-      [table, id],
-    );
-    next();
-  } catch (error) {
-    if (error instanceof QueryResultError) {
-      res.status(404).json({ error: `${table} not found` });
-    } else {
-      next(error);
-    }
-  }
-}
+// TODO: factor out common code in addEntity, addAction, removeEntity, removeAction
 
 async function getAll(req, res, next) {
   try {
@@ -274,7 +255,6 @@ async function vote(req, res, next) {
 }
 
 export default {
-  validateParameter,
   getAll,
   getOne,
   createOne,
